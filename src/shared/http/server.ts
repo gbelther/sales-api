baa7e0e8edfecc8d9860dba7d8400c79ee89d1fd
gbelther <belthergabriel@gmail.com>
2021-10-12 19:03:07 -0300

    Importacao do express async errors no server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
-
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
