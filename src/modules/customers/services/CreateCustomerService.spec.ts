2c6899de783b379da11d0f464dc9d68de75787ba
gbelther <belthergabriel@gmail.com>
2021-10-17 17:05:00 -0300

    Teste do CreateCustomerService

---
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+import FakeCustomersRepository from '../domain/repositories/fakes/FakeCustomersRepository';
+import CreateCustomerService from './CreateCustomerService';
+
+describe('CreateCustomer', () => {
+  it('should be able to create a new customer', async () => {
+    const fakeCustomersRepository = new FakeCustomersRepository();
+
+    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
+
+    const customer = await createCustomer.execute({
+      name: 'Gabriel',
+      email: 'gabriel@email.com',
+    });
+
+    expect(customer).toHaveProperty('id');
+  });
+
+  it('should not be able to create two customers with the same email', () => {
+    expect(1).toBe(1);
+  });
+});
