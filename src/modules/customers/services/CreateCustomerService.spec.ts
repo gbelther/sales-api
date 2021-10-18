15a92f080a0885e957de5958cd41f967bd8f54b6
gbelther <belthergabriel@gmail.com>
2021-10-18 20:33:05 -0300

    Refatorando os teste do serviço createcustomerservice

--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -3,12 +3,16 @@ import 'reflect-metadata';
 import FakeCustomersRepository from '../domain/repositories/fakes/FakeCustomersRepository';
 import CreateCustomerService from './CreateCustomerService';
 
+let fakeCustomersRepository: FakeCustomersRepository;
+let createCustomer: CreateCustomerService;
+
 describe('CreateCustomer', () => {
+  beforeEach(() => {
+    fakeCustomersRepository = new FakeCustomersRepository();
+    createCustomer = new CreateCustomerService(fakeCustomersRepository);
+  });
+
   it('should be able to create a new customer', async () => {
-    const fakeCustomersRepository = new FakeCustomersRepository();
-
-    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
-
     const customer = await createCustomer.execute({
       name: 'Gabriel',
       email: 'gabriel@email.com',
@@ -18,10 +22,6 @@ describe('CreateCustomer', () => {
   });
 
   it('should not be able to create two customers with the same email', async () => {
-    const fakeCustomersRepository = new FakeCustomersRepository();
-
-    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
-
     await createCustomer.execute({
       name: 'Gabriel',
       email: 'gabriel@email.com',
